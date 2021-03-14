1320438e3113260fb5228a9c6b2b54ab63151ec6
Blayne Chard <blayne@chard.com>
2021-03-15 12:52:49 +1300

    fix: correct folder names for data generation

--- make-data.sh
+++ make-data.sh
@@ -2,30 +2,28 @@
 
 # Generate a bunch of random test data
 
-mkdir -p data/small
-mkdir -p data/big
+mkdir -p data/16
+dd if=/dev/urandom of=data/16/00 bs=1M count=256
+dd if=/dev/urandom of=data/16/01 bs=1M count=256
+dd if=/dev/urandom of=data/16/02 bs=1M count=256
+dd if=/dev/urandom of=data/16/03 bs=1M count=256
+dd if=/dev/urandom of=data/16/04 bs=1M count=256
+dd if=/dev/urandom of=data/16/05 bs=1M count=256
+dd if=/dev/urandom of=data/16/06 bs=1M count=256
+dd if=/dev/urandom of=data/16/07 bs=1M count=256
+dd if=/dev/urandom of=data/16/08 bs=1M count=256
+dd if=/dev/urandom of=data/16/09 bs=1M count=256
+dd if=/dev/urandom of=data/16/10 bs=1M count=256
+dd if=/dev/urandom of=data/16/11 bs=1M count=256
+dd if=/dev/urandom of=data/16/12 bs=1M count=256
+dd if=/dev/urandom of=data/16/13 bs=1M count=256
+dd if=/dev/urandom of=data/16/14 bs=1M count=256
+dd if=/dev/urandom of=data/16/15 bs=1M count=256
+dd if=/dev/urandom of=data/16/16 bs=1M count=256
+dd if=/dev/urandom of=data/16/17 bs=1M count=256
+dd if=/dev/urandom of=data/16/18 bs=1M count=256
+dd if=/dev/urandom of=data/16/19 bs=1M count=256
 
-dd if=/dev/urandom of=data/small/00 bs=1M count=256
-dd if=/dev/urandom of=data/small/01 bs=1M count=256
-dd if=/dev/urandom of=data/small/02 bs=1M count=256
-dd if=/dev/urandom of=data/small/03 bs=1M count=256
-dd if=/dev/urandom of=data/small/04 bs=1M count=256
-dd if=/dev/urandom of=data/small/05 bs=1M count=256
-dd if=/dev/urandom of=data/small/06 bs=1M count=256
-dd if=/dev/urandom of=data/small/07 bs=1M count=256
-dd if=/dev/urandom of=data/small/08 bs=1M count=256
-dd if=/dev/urandom of=data/small/09 bs=1M count=256
-dd if=/dev/urandom of=data/small/10 bs=1M count=256
-dd if=/dev/urandom of=data/small/11 bs=1M count=256
-dd if=/dev/urandom of=data/small/12 bs=1M count=256
-dd if=/dev/urandom of=data/small/13 bs=1M count=256
-dd if=/dev/urandom of=data/small/14 bs=1M count=256
-dd if=/dev/urandom of=data/small/15 bs=1M count=256
-dd if=/dev/urandom of=data/small/16 bs=1M count=256
-dd if=/dev/urandom of=data/small/17 bs=1M count=256
-dd if=/dev/urandom of=data/small/18 bs=1M count=256
-dd if=/dev/urandom of=data/small/19 bs=1M count=256
-
-
-dd if=/dev/urandom of=data/big/00 bs=1M count=2048
-dd if=/dev/urandom of=data/big/01 bs=1M count=2048
+mkdir -p data/2
+dd if=/dev/urandom of=data/2/00 bs=1M count=2048
+dd if=/dev/urandom of=data/2/01 bs=1M count=2048
