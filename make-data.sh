efb00be36ce5ae01921278e43c26d797ec0a839f
Blayne Chard <blayne@chard.com>
2021-03-15 12:57:11 +1300

    fix: only generate 4GB of data

--- make-data.sh
+++ make-data.sh
@@ -19,10 +19,6 @@ dd if=/dev/urandom of=data/16/12 bs=1M count=256
 dd if=/dev/urandom of=data/16/13 bs=1M count=256
 dd if=/dev/urandom of=data/16/14 bs=1M count=256
 dd if=/dev/urandom of=data/16/15 bs=1M count=256
-dd if=/dev/urandom of=data/16/16 bs=1M count=256
-dd if=/dev/urandom of=data/16/17 bs=1M count=256
-dd if=/dev/urandom of=data/16/18 bs=1M count=256
-dd if=/dev/urandom of=data/16/19 bs=1M count=256
 
 mkdir -p data/2
 dd if=/dev/urandom of=data/2/00 bs=1M count=2048
